1de30161eea06662dadd1e571176a8e038a4c4b8
tsbubonic <7264@trinity.croydon.sch.uk>
2025-02-24 11:08:46 +0000

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+#comment
+#pyhton ignores comments
+#comments explain code
+#the best programmers comment all their code
+#the worst programmers dont comment their code
+
+name = "dave" #variable called name
+print(name)#displays name 
