959ecb52a676aa5b68fc326d9358549a75fa0a7e
tsbubonic <7264@trinity.croydon.sch.uk>
2025-03-03 11:27:48 +0000

    Add files via upload

---
+++ mad lib.py
@@ -0,0 +1,14 @@
+#madlib
+
+print("welcome to mad lib")
+print("")
+
+place1 = input("enter a place >>>")
+person1 = input("enter a person >>>")
+adjective1 = input("enter a adjective >>>")
+noun1 = input("enter a noun >>>")
+verb1 = input("enter a verb >>>")
+noun2 = input("enter a noun >>>")
+print("in",place1,"me and", person1,"love talking about how",adjective1,noun1, "is.")
+print("sometimes, me and", person1, verb1, "in", place1)
+print("we always steal each others", noun2, person1, "doesnt like that")
