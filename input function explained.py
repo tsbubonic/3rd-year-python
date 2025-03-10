ce04e2a206b20dc70fc9ffff544c914df4a472bc
tsbubonic <7264@trinity.croydon.sch.uk>
2025-03-10 10:43:45 +0000

    Add files via upload

---
+++ input function explained.py
@@ -0,0 +1,15 @@
+#input function
+#other functions
+print("inputs")
+bin(67)
+hex(67)
+#they all have brackets + purple
+#input()
+#allows a user to enter data into a program
+#via the keyboard
+#data entered by the input function is stored...
+#...in a variable
+
+name = input("enter name: >>>")
+print("hello", name)
+print("so", name, "how are you")
