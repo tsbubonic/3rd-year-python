4d6c8920eeda1db4e3131cff5cf516343e710198
tsbubonic <7264@trinity.croydon.sch.uk>
2025-03-10 10:43:17 +0000

    Add files via upload

---
+++ variables + input challenge.py
@@ -0,0 +1,24 @@
+name = input("enter your name>>")
+print("hello", name)
+
+age = input("enter your age>>")
+print("you're", age)
+
+color = input("enter your fav color>>")
+print("i like", color, "too")
+
+team = input("enter your fav sports team>>")
+print(team, "is ok this season")
+
+food = input("enter your fav food>>")
+print("fried chicken is better than", food)
+
+game = input("enter your fav game>>")
+print("im better than you at", game)
+
+weight = input("enter your weight>>")
+
+height = input("enter your height>>")
+
+print(weight, "at", height, "kinda fat fr")
+
